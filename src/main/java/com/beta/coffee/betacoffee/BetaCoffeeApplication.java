1ef2158a5079bbee8f26b6dfb55937300eee047b
aguiarpo <eduardo.poerner@catolicasc.org.br>
2022-05-28 12:18:10 -0300

    Initial commit

---
+++ src/main/java/com/beta/coffee/betacoffee/BetaCoffeeApplication.java
@@ -0,0 +1,13 @@
+package com.beta.coffee.betacoffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetaCoffeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetaCoffeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beta/coffee/betacoffee/BetaCoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beta.coffee.betacoffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetaCoffeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
