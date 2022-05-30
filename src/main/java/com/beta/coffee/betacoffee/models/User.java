058a710dc746a9ecd18b72f6f05ecbd296685e4d
aguiarpo <eduardo.poerner@catolicasc.org.br>
2022-05-30 16:49:12 +0000

    Removido UserLevelsOfAccess

--- src/main/java/com/beta/coffee/betacoffee/models/User.java
+++ src/main/java/com/beta/coffee/betacoffee/models/User.java
@@ -2,11 +2,25 @@ package com.beta.coffee.betacoffee.models;
 
 import javax.persistence.*;
 
+import com.beta.coffee.betacoffee.models.enums.LevelsOfAccess;
+
+import lombok.Getter;
+import lombok.Setter;
+
 @Entity
 @SequenceGenerator(name = "user_seq", sequenceName = "user_seq",
         initialValue = 1, allocationSize = 1)
 @Table(name="`User`")
+@Getter
+@Setter
 public class User{
+
+    public User(String nameUser, String email, String password, LevelsOfAccess type){
+        this.nameUser = nameUser;
+        this.email = email;
+        this.password = password;
+        this.type = type;
+    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq")
@@ -18,7 +32,8 @@ public class User{
 
     String password;
 
-    @ManyToOne
-    UserLevelAccess levelAccess;
+    @Enumerated(EnumType.STRING)
+    @Column(unique = true)
+    LevelsOfAccess type;
 
 }
