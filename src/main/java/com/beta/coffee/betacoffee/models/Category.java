abdeb1336925f9d47672beb635f0f2eea6887bbc
aguiarpo <eduardo.poerner@catolicasc.org.br>
2022-05-30 21:42:26 +0000

    Trocado Byte por Short e Lombok

--- src/main/java/com/beta/coffee/betacoffee/models/Category.java
+++ src/main/java/com/beta/coffee/betacoffee/models/Category.java
@@ -3,6 +3,11 @@ package com.beta.coffee.betacoffee.models;
 import java.util.List;
 import javax.persistence.*;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 @Entity
 @SequenceGenerator(name = "category_seq", sequenceName = "category_seq",
         initialValue = 1, allocationSize = 1)
@@ -10,8 +15,9 @@ public class Category{
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "category_seq")
-    Long id;
+    Short id;
 
+    @Column(unique = true)
     String name;
     
     @OneToMany(mappedBy="category")
