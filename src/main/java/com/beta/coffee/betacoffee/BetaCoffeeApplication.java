2a64cdb0cae4da1dd9ce7e4b0fb2954260522bec
aguiarpo <eduardo.poerner@catolicasc.org.br>
2022-05-30 16:47:21 +0000

    PostConstuct Timezone = São Paulo

--- src/main/java/com/beta/coffee/betacoffee/BetaCoffeeApplication.java
+++ src/main/java/com/beta/coffee/betacoffee/BetaCoffeeApplication.java
@@ -1,10 +1,19 @@
 package com.beta.coffee.betacoffee;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BetaCoffeeApplication {
+
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(BetaCoffeeApplication.class, args);
