6fa9058ce41d317b9324eebeb0843c4026e0a017
aguiarpo <eduardo.poerner@catolicasc.org.br>
2022-05-30 23:16:20 +0000

    Editado Lombok

--- src/main/java/com/beta/coffee/betacoffee/models/Product.java
+++ src/main/java/com/beta/coffee/betacoffee/models/Product.java
@@ -5,6 +5,11 @@ import java.util.List;
 import javax.persistence.*;
 import com.beta.coffee.betacoffee.models.enums.CupSize;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 @Entity
 @SequenceGenerator(name = "product_seq", sequenceName = "product_seq",
         initialValue = 1, allocationSize = 1)
