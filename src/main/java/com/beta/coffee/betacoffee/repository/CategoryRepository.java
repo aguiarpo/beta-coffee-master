1484609f960ba734b28b003feef103c1febf2306
aguiarpo <eduardo.poerner@catolicasc.org.br>
2022-05-30 22:17:09 +0000

    Native Query findByNameLike

--- src/main/java/com/beta/coffee/betacoffee/repository/CategoryRepository.java
+++ src/main/java/com/beta/coffee/betacoffee/repository/CategoryRepository.java
@@ -1,9 +1,15 @@
 package com.beta.coffee.betacoffee.repository;
 
 import com.beta.coffee.betacoffee.models.Category;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface CategoryRepository extends PagingAndSortingRepository<Category, Long> {
     Category findById(Short id);
-    Category findByName(String name);
+    @Query(value = "SELECT * FROM category where name like :name%",
+    nativeQuery = true)
+    Page<Category> findByNameLike(String name, Pageable pageable);
 } 
