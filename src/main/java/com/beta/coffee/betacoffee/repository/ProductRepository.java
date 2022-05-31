5d1796a52796a2a5d46dc46e7537e6d3f21e1061
aguiarpo <eduardo.poerner@catolicasc.org.br>
2022-05-31 00:52:53 +0000

    Remove findByCode

--- src/main/java/com/beta/coffee/betacoffee/repository/ProductRepository.java
+++ src/main/java/com/beta/coffee/betacoffee/repository/ProductRepository.java
@@ -8,9 +8,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface ProductRepository extends PagingAndSortingRepository<Product, Long> {
-    @Query(value = "SELECT * FROM product where id = :id",
-    nativeQuery = true)
-    Product findByCode(Long id);
     @Query(value = "SELECT * FROM product where name like :name%",
     nativeQuery = true)
     Page<Product> findByNameLike(String name, Pageable pageable);
