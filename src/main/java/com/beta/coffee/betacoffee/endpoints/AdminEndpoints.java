e278da55b763afea6be720c7ac58ac9a7fe216f7
aguiarpo <eduardo.poerner@catolicasc.org.br>
2022-05-31 17:43:12 +0000

    Enviando Email - Teste

--- src/main/java/com/beta/coffee/betacoffee/endpoints/AdminEndpoints.java
+++ src/main/java/com/beta/coffee/betacoffee/endpoints/AdminEndpoints.java
@@ -1,15 +1,17 @@
 package com.beta.coffee.betacoffee.endpoints;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.beta.coffee.betacoffee.models.User;
 import com.beta.coffee.betacoffee.models.enums.LevelsOfAccess;
 import com.beta.coffee.betacoffee.repository.UserRepository;
-import com.beta.coffee.betacoffee.twilio.TwilioMessageWhatsapp;
+import com.beta.coffee.betacoffee.message.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.MailException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,10 +21,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class AdminEndpoints{
 
     private final UserRepository userDao;
+    private Mailer mailer;
 
     @Autowired
-    public AdminEndpoints(UserRepository userDao) {
+    public AdminEndpoints(UserRepository userDao, Mailer mailer) {
         this.userDao = userDao;
+        this.mailer = mailer;
     }
 
     @GetMapping(path = "create/admin")
@@ -42,5 +46,24 @@ public class AdminEndpoints{
         Integer error = TwilioMessageWhatsapp.
         messageSend("whatsapp:+554791164298", "Oi, gostaria de dar seu Cu?");
         return new ResponseEntity<>(error, HttpStatus.OK);
+    }
+
+    @GetMapping(path = "message/email")
+    public ResponseEntity<?> email(){
+        ArrayList<String> recipients = new ArrayList<>();
+        recipients.add("eduardo.aguiarpo@gmail.com");
+        sendEmail(recipients, "12345678");
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    private Integer sendEmail(ArrayList<String> recipients, String body){
+        try{
+            mailer.submit(new EmailMessage("Suporte <suporte.animais.guaramirim@gmail.com>",
+                    recipients, "Senha", "Senha -> " + body));
+            return 1;
+        }catch (MailException ignored){
+            System.out.println(ignored + "erro");
+            return 0;
+        }
     }
 } 
