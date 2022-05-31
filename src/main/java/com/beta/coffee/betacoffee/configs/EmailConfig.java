3c9bca2b8a37422966e6ca3bdb27ebe22414fa7a
aguiarpo <eduardo.poerner@catolicasc.org.br>
2022-05-31 17:42:42 +0000

    Adicionado Email

---
+++ src/main/java/com/beta/coffee/betacoffee/configs/EmailConfig.java
@@ -0,0 +1,44 @@
+package com.beta.coffee.betacoffee.configs;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+import java.util.Properties;
+
+@Configuration
+@Service
+@PropertySource("classpath:/mail.properties")
+public class EmailConfig { // Classe que configura o email
+
+    private final Environment env;
+
+    @Autowired
+    public EmailConfig(Environment env) {
+        this.env = env;
+    }
+
+    public JavaMailSender mailSender(){
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+        mailSender.setHost(env.getProperty("spring.mail.host"));
+        mailSender.setPort(Integer.parseInt(Objects.requireNonNull(env.getProperty("spring.mail.port"))));
+        mailSender.setUsername(env.getProperty("spring.mail.username"));
+        mailSender.setPassword(env.getProperty("spring.mail.password"));
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.connectiontimeout", 10000);
+
+        mailSender.setJavaMailProperties(props);
+
+        return mailSender;
+    }
+}
---
+++ src/main/java/com/beta/coffee/betacoffee/message/Mailer.java
@@ -0,0 +1,30 @@
+package com.beta.coffee.betacoffee.message;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Mailer  { // Classe de configuração do email
+
+    private JavaMailSender javaMailSender;
+
+    @Autowired
+    public Mailer(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    public void submit(EmailMessage emailMessage) throws MailException {
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+
+        simpleMailMessage.setFrom(emailMessage.getSender());
+        simpleMailMessage.setTo(emailMessage.getRecipients()
+                .toArray(new String[0]));
+        simpleMailMessage.setSubject(emailMessage.getSubject());
+        simpleMailMessage.setText(emailMessage.getBody());
+
+        javaMailSender.send(simpleMailMessage);
+    }
+}
