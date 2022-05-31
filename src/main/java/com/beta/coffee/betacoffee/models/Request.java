77f6178404e317efb14b6317ca1987e948fbcede
aguiarpo <eduardo.poerner@catolicasc.org.br>
2022-05-31 00:52:09 +0000

    Lombok e nameUser para name

--- src/main/java/com/beta/coffee/betacoffee/models/Request.java
+++ src/main/java/com/beta/coffee/betacoffee/models/Request.java
@@ -3,16 +3,21 @@ package com.beta.coffee.betacoffee.models;
 import java.util.List;
 import javax.persistence.*;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Entity
 @SequenceGenerator(name = "request_seq", sequenceName = "request_seq",
         initialValue = 1, allocationSize = 1)
+@Getter
+@Setter
 public class Request{
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "request_seq")
     Long id;
 
-    String nameUser;
+    String name;
 
     String email;
 
